4ddf5c4320dacb76f7d6cb32f9123469aa099f85
Kalenyák <kalanovics.z@gmail.com>
2017-10-30 12:47:02 +0100

    script,animation
    added isodiametric camerafollow script, and player hitting animation

--- Assets/Scripts/Camera/IsoCameraFollow.cs
+++ Assets/Scripts/Camera/IsoCameraFollow.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour {
+public class IsoCameraFollow : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Transform target; //player
+    public float smoothing = 5f;
+    Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - target.position;
+
+   
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
 }
